1cdfcc07db880bbfbfbfe2dfbfc72edd61ef3b32
audrealdanskin <audreadanskin@me.com>
2026-04-30 16:08:24 -0500

    Adding load balancer

--- main.tf
+++ main.tf
@@ -24,3 +24,25 @@ resource "vultr_vpc" "my_vpc" {
     description = "my vpc"
     region = "ord"
 }
+resource "vultr_load_balancer" "lb" {
+  region = "ord"
+  label     = "vultr-load-balancer"
+  balancing_algorithm = "roundrobin"
+
+  forwarding_rules {
+    frontend_protocol = "http"
+    frontend_port = 82
+    backend_protocol = "http"
+    backend_port = 81
+  }
+
+  health_check {
+    path = "/test"
+    port = 8080
+    protocol = "http"
+    response_timeout = 1
+    unhealthy_threshold =2 
+    check_interval = 3
+    healthy_threshold =4
+  }
+}
